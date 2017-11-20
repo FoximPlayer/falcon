f762a813a41ebe7593dbeaba096b612458980365
FoximPlayer <fox.h@o2.pl>
2017-11-20 15:22:21 +0100

    new version with memory check

--- monitor.py
+++ monitor.py
@@ -1,39 +1,17 @@
-# sample.py
-
 import falcon
-
-
 from psutil import virtual_memory
-
-mem = virtual_memory()
-total = mem.total  # total physical memory available
 
 
 class MemResource:
     def on_get(self, req, resp):
-        """Handles GET requests"""
+        mem = virtual_memory()
         response = {
-            'total': total
+            'free': mem.free,
+            'total': mem.total
         }
-
         resp.media = response
 
-
-class QuoteResource:
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        quote = {
-            'quote': (
-                "I've always been more interested in "
-                "the future than in the past."
-            ),
-            'author': 'Grace Hopper'
-        }
-
-        resp.media = quote
-
 api = falcon.API()
-api.add_route('/quote', QuoteResource())
 api.add_route('/mem', MemResource())
 
 
